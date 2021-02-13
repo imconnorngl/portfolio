846bb823e6034e5f1d10136a651d97537bc01b11
imconnorngl <developer@purpled.host>
2021-02-13 12:42:28 +0000

    oops

--- js/search.js
+++
@@ -1,7 +0,0 @@
-const navSearch = document.getElementById("navSearch")
-const navInput = document.getElementById("player")
-
-navSearch.onsubmit = event => {
-    event.preventDefault();
-    if(navInput.value) window.location.href = `stats.html?player=${navInput.value}`;
-}
