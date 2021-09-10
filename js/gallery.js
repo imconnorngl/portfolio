be9898610715b0b9c9e293614229ecf4a5528dd0
Connor Lewis <50084106+imconnorngl@users.noreply.github.com>
2021-09-10 09:48:13 +0100

    revamp gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,53 +1,35 @@
 var index = 0;
-
-var locations = [
-  { display: "ELO Bot: Pending Score Request", asset: "img/gallery/1.png" }, 
-  { display: "ELO Bot: Queue Filling", asset: "img/gallery/2.png" },
-  { display: "ELO Bot: Team Picking", asset: "img/gallery/3.png" },
-  { display: "Statsify (Hypixel Stat Bot): Ping", asset: "img/gallery/4.png" },
-  { display: "Bot Application Showcase", asset: "img/gallery/5.png" },
-  { display: "General Bot: Quick Math", asset: "img/gallery/6.png" },
-  { display: "ELO Bot: Accepted Score Request", asset: "img/gallery/7.png" },
-  { display: "Statsify (Hypixel Stat Bot): Bedwars Profile Command", asset: "img/gallery/8.png" },
-  { display: "ELO Bot: Profile Command",  asset: "img/gallery/9.png" },
-]
-
-for (i = 0; i < locations.length; i++) $("#dotsContainer").append(`<span onclick="showSlide(${i})" class="dots"></span>`)
-showSlide(0);
+const childrenCount = $("#slide-container > img").length - 1
 
 function showSlide(position) {
-  if (position > locations.length - 1) index = 0;
-  else if (position < 0) index = locations.length - 1;
-  else index = position;
+  index = position;
 
-  for (i = 0; i < locations.length; i++) $(".dots").eq(i).removeClass("active")
-  $(".dots").eq(index).addClass("active")
+  $(".dots").each(function (i) {
+    if(i == index) $(this).addClass("active")
+    else $(this).removeClass("active")
+  })
 
-  const location = locations[index]
-
-  if(location.asset.includes(".mp4")) {
-    $(".slide.video").css("display", "block")
-    $(".slide.img").css("display", "none")
-    $(".slide.video").find("source").attr("src", location.asset)
-    $(".slide.video")[0].load()
-  } else {
-    $(".slide.img").css("display", "block")
-    $(".slide.video").css("display", "none")
-    $(".slide.img").attr("src", location.asset)
-  }
-
-  $("#text-bar").find("a").html(location.display)
-  
-  if(location.url) $("#text-bar").find("a").attr("href", location.url)
-  else $("#text-bar").find("a").removeAttr("href")
+  $("#slide-container > .slide").each(function (i) { 
+    if(i == index) {
+      $(this).css("display", "block")
+      $("#text-bar").find("a").html($(this).attr("alt"))
+    } else {
+      $(this).css("display", "none")
+    }
+  })
 }
 
+for (i = 0; i <= childrenCount; i++) $("#dotsContainer").append(`<span onclick="showSlide(${i})" class="dots"></span>`)
+showSlide(0)
+
 $("#next").on("click", function() {
-  index += 1;
-  showSlide(index);
-})
+  if(index == childrenCount) index = 0
+  else index += 1;
+  showSlide(index)
+});
 
 $("#previous").on("click", function() {
-  index -= 1;
-  showSlide(index);
-})
+  if(index == 0) index = childrenCount
+  else index -= 1;
+  showSlide(index)
+});
